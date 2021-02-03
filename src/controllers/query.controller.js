2980b4d66f7adf92dcff0b7f554fe896f427ed75
trongnv <trongnv138@gmail.com>
2021-02-03 11:38:43 +0700

    format

--- src/controllers/query.controller.js
+++ src/controllers/query.controller.js
@@ -24,39 +24,39 @@ module.exports = {
     }
   },
   getToken: async (req, res, next) => {
-		try {
+    try {
       console.log(req.params.id)
       const file = await FileModel.findById(req.params.id);
       console.log(file)
-			if (file) {
-				const secretKey = process.env.JWT_TOKEN_STORAGE;
-				const token = jwt.sign({
-					exp: Math.floor(Date.now() / 1000) + (1 * 60),
-					data: {
-						_id: file._id
-					}
-				}, secretKey);
-				const HOST = process.env.HOST;
-				const url = `${HOST}/api/v1/storage/download-by-token/${file._id}?key=${token}`;
-				res.json({
-					success: true,
-					data: {
-						url
-					},
-				});
-			} else {
-				res.status(400).json({
-					success: false,
-					message: 'File does not exist!',
-				});
-			}
-		} catch (error) {
-			console.log('error: ', error);
-			res.status(400).json({
-				success: false,
-				error,
-				message: 'Unable to get data with provided.',
-			});
-		}
-	},
+      if (file) {
+        const secretKey = process.env.JWT_TOKEN_STORAGE;
+        const token = jwt.sign({
+          exp: Math.floor(Date.now() / 1000) + (1 * 60),
+          data: {
+            _id: file._id
+          }
+        }, secretKey);
+        const HOST = process.env.HOST;
+        const url = `${HOST}/api/v1/storage/download-by-token/${file._id}?key=${token}`;
+        res.json({
+          success: true,
+          data: {
+            url
+          },
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          message: 'File does not exist!',
+        });
+      }
+    } catch (error) {
+      console.log('error: ', error);
+      res.status(400).json({
+        success: false,
+        error,
+        message: 'Unable to get data with provided.',
+      });
+    }
+  },
 }
--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,25 +2,25 @@ const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-	const id = req.params.id;
-	const token = req.query.key;
-	if (!token) {
-		return next(
-			createError(401, { status: 401, message: "Unauthorized" })
-		);
-	}
-	let decoded;
-	try {
-		decoded = jwt.verify(token, process.env.JWT_TOKEN_STORAGE);
-	} catch (err) {
-		return next(
-			createError(401, { status: 401, message: "Unauthorized" })
-		);
-	}
-	if (!decoded || !decoded.data || id !== decoded.data._id) {
-		return next(
-			createError(404, { status: 404, message: "File not found" })
-		);
-	}
-	next();
+  const id = req.params.id;
+  const token = req.query.key;
+  if (!token) {
+    return next(
+      createError(401, { status: 401, message: "Unauthorized" })
+    );
+  }
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_TOKEN_STORAGE);
+  } catch (err) {
+    return next(
+      createError(401, { status: 401, message: "Unauthorized" })
+    );
+  }
+  if (!decoded || !decoded.data || id !== decoded.data._id) {
+    return next(
+      createError(404, { status: 404, message: "File not found" })
+    );
+  }
+  next();
 };
